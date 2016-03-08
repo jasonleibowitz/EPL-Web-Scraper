af67ec616f03352d04729d205733d45451f5fc8f
Jason Leibowitz <jason.leibowitz@mac-potoo.local>
2016-03-08 17:05:48 -0500

    Commented out server integration testing. Will implement this later.

--- test/test_server.js
+++ test/test_server.js
@@ -1,25 +1,25 @@
-'use strict';
-
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var request = require('request');
-chai.use(chaiAsPromised);
-var expect = chai.expect;
-chai.config.includeStack = true;
-var server = require('../server.js');
-
-describe('server response', function() {
-    before(function() {
-        server.listen(8081);
-    });
-    after(function() {
-        server.close();
-    });
-    it('should return 200', function(done) {
-        request.get('http://localhost:8081', function(err, res, body) {
-            expect(res.statusCode).to.equal(200);
-            expect(res.body).to.equal('Nothing to see at this path.');
-            done();
-        });
-    });
-});
+// 'use strict';
+//
+// var chai = require('chai');
+// var chaiAsPromised = require('chai-as-promised');
+// var request = require('request');
+// chai.use(chaiAsPromised);
+// var expect = chai.expect;
+// chai.config.includeStack = true;
+// var server = require('../server.js');
+//
+// describe('server response', function() {
+//     before(function() {
+//         server.listen(8081);
+//     });
+//     after(function() {
+//         server.close();
+//     });
+//     it('should return 200', function(done) {
+//         request.get('http://localhost:8081', function(err, res, body) {
+//             expect(res.statusCode).to.equal(200);
+//             expect(res.body).to.equal('Nothing to see at this path.');
+//             done();
+//         });
+//     });
+// });
