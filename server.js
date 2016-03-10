01a9c20a355c0b3a037f27642bd6cf4c6ed35154
Jason Leibowitz <jason.leibowitz@mac-potoo.local>
2016-03-10 16:45:11 -0500

    Started building loop for all 3 competitions in get fixtures path

--- server.js
+++ server.js
@@ -279,75 +279,90 @@ app.get('/update-results', function(req, res) {
 app.get('/update-fixtures', function(req, res) {
 
     console.log('in update fixtures');
-    var premierLeaugeUrl = 'http://www.bbc.com/sport/football/premier-league/fixtures';
-    var championsLeagueUrl = 'http://www.bbc.com/sport/football/champions-league/fixtures';
-    var europaLeagueUrl = 'http://www.bbc.com/sport/football/europa-league/fixtures';
 
-    request(premierLeaugeUrl, function(error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-            var start_time = new Date();
+    var competitions = [
+        {
+            competitionName: 'Premier League',
+            url: 'http://www.bbc.com/sport/football/premier-league/fixtures'
+        },
+        {
+            competitionName: 'Champions League',
+            url: 'http://www.bbc.com/sport/football/champions-league/fixtures'
+        },
+        {
+            competitionName: 'Europa League',
+            url: 'http://www.bbc.com/sport/football/europa-league/fixtures'
+        }
+    ];
 
-            $(".fixtures-table").children('table').filter(function() {
-                var data = $(this);
+    competitions.forEach(function(competition, index) {
+        request(competition.url, function(error, response, html) {
+            if (!error) {
+                var $ = cheerio.load(html);
+                var start_time = new Date();
 
-                data.each(function(index, element) {
+                $(".fixtures-table").children('table').filter(function() {
+                    var data = $(this);
 
-                    $(element).find('tbody').children().each(function(index, element) {
+                    data.each(function(index, element) {
 
-                        var matchDate = DataNormalizer.cleanDate($(element).parents('.table-stats').prevAll('h2').first().text().trim());
-                        var kickoffHours = $(element).find('.kickoff').text().trim().split(':')[0];
-                        var kickoffMinutes = $(element).find('.kickoff').text().trim().split(':')[1];
-                        var homeTeam = $(element).find('.match-details').find('.team-home').text().trim();
-                        var awayTeam = $(element).find('.match-details').find('.team-away').text().trim();
-                        var homeTeamGoals = $(element).find('.match-details').find('.score').text().trim().split('-')[0];
-                        var awayTeamGoals = $(element).find('.match-details').find('.score').text().trim().split('-')[1];
-                        var matchStatus;
+                        $(element).find('tbody').children().each(function(index, element) {
 
-                        if ($(element).hasClass('preview')) {
-                            matchStatus = 'Scheduled';
-                        } else if ($(element).hasClass('live')) {
-                            matchStatus = 'In Progress';
-                        } else if ($(element).hasClass('report')) {
-                            matchStatus = 'Finished';
-                        } else {
-                            matchStatus = 'Postponed';
-                        }
+                            var matchDate = DataNormalizer.cleanDate($(element).parents('.table-stats').prevAll('h2').first().text().trim());
+                            var kickoffHours = $(element).find('.kickoff').text().trim().split(':')[0];
+                            var kickoffMinutes = $(element).find('.kickoff').text().trim().split(':')[1];
+                            var homeTeam = $(element).find('.match-details').find('.team-home').text().trim();
+                            var awayTeam = $(element).find('.match-details').find('.team-away').text().trim();
+                            var homeTeamGoals = $(element).find('.match-details').find('.score').text().trim().split('-')[0];
+                            var awayTeamGoals = $(element).find('.match-details').find('.score').text().trim().split('-')[1];
+                            var matchStatus;
 
-                        // Add kickoff time to matchDate
-                        matchDate.setUTCHours(kickoffHours);
-                        matchDate.setMinutes(kickoffMinutes);
+                            if ($(element).hasClass('preview')) {
+                                matchStatus = 'Scheduled';
+                            } else if ($(element).hasClass('live')) {
+                                matchStatus = 'In Progress';
+                            } else if ($(element).hasClass('report')) {
+                                matchStatus = 'Finished';
+                            } else {
+                                matchStatus = 'Postponed';
+                            }
 
-                        if (homeTeamGoals === "P") {
-                            // match postponed. do nothing
-                            console.log('Before save. Home: ' + homeTeam + ' away: ' + awayTeam + '. Score: ' + homeTeamGoals + ' to ' + awayTeamGoals);
-                        } else {
-                            Fixture.findOneAndUpdate({
-                                date: matchDate,
-                                competition: 'Premier League',
-                                homeTeam: homeTeam,
-                                awayTeam: awayTeam,
-                            }, {
-                                date: matchDate,
-                                status: matchStatus,
-                                homeTeam: homeTeam,
-                                awayTeam: awayTeam,
-                                updated: Date.now()
-                            }, {
-                                upsert: true
-                            }, function(err, result) {
-                                if (err) {
-                                    console.log('error at ', matchDate);
-                                    throw err;
-                                }
-                                console.log('successfully created ' + matchDate + ' match between ' + homeTeam + ' vs ' + awayTeam);
-                            });
-                        }
+                            // Add kickoff time to matchDate
+                            matchDate.setUTCHours(kickoffHours);
+                            matchDate.setMinutes(kickoffMinutes);
+
+                            if (homeTeamGoals === "P") {
+                                // match postponed. do nothing
+                                console.log('Before save. Home: ' + homeTeam + ' away: ' + awayTeam + '. Score: ' + homeTeamGoals + ' to ' + awayTeamGoals);
+                            } else {
+                                Fixture.findOneAndUpdate({
+                                    date: matchDate,
+                                    competition: competition.competitionName,
+                                    homeTeam: homeTeam,
+                                    awayTeam: awayTeam,
+                                }, {
+                                    date: matchDate,
+                                    competition: competition.competitionName,
+                                    status: matchStatus,
+                                    homeTeam: homeTeam,
+                                    awayTeam: awayTeam,
+                                    updated: Date.now()
+                                }, {
+                                    upsert: true
+                                }, function(err, result) {
+                                    if (err) {
+                                        console.log('error at ', matchDate);
+                                        throw err;
+                                    }
+                                    console.log('successfully created ' + matchDate + ' match between ' + homeTeam + ' vs ' + awayTeam);
+                                });
+                            }
+                        });
                     });
                 });
-            });
-            var end_time = new Date();
-        };
+                var end_time = new Date();
+            };
+        });
     });
 
     res.send('check console');
